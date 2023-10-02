a91cc574fdd8b3e400bc8c898cb17c81875c0680
RezoanulHasan <rezoanulhasan96@gmail.com>
2023-10-02 17:11:59 +0600

    add express setup

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+var cors = require('cors')
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.get('/', (req, res) => {
+    res.send(' Car-Hub  Server is running........')
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
